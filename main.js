3532222d147fab69aa12f9785886157f89c03142
Andreas Nugroho <andreas.nugroho@ti.ukdw.ac.id>
2024-02-28 21:08:31 +0700

    set message

--- main.js
+++ main.js
@@ -7,9 +7,23 @@ const options = {
 
 const gempabot = new TelegramBot(token, options)
 
-gempabot.on("message", (callback) => {
-    const id = callback.from.id
-    gempabot.sendMessage(id,"Iya")
+// gempabot.on("message", (callback) => {
+//     const id = callback.from.id
+//     gempabot.sendMessage(id,callback.text)
+// })
+
+const prefix = "."
+
+const sayHi = new RegExp(`^${prefix}halo$`)
+const gempa = new RegExp(`^${prefix}gempa$`)
+
+
+gempabot.onText(sayHi, (callback) => {
+    gempabot.sendMessage(callback.from.id,"Hallo Juga!!")
+})
+
+gempabot.onText(gempa, (callback) => {
+    gempabot.sendMessage(callback.from.id,"Ini Berita Gempa")    
 })
 
 // console.log('bot ready! ')
