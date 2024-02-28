ffe80cdcfc1a36fe6b8ee7acb041f3a901e3c3c5
Andreas Nugroho <andreas.nugroho@ti.ukdw.ac.id>
2024-02-28 20:53:43 +0700

    create polling for development

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 const TelegramBot = require("node-telegram-bot-api")
 
 const token = "7033442256:AAHL6SjDzUE2PNdShyT-h9qtqR5fm1XZ1u8"
+const options = {
+    polling:true
+}
+
+const gempabot = new TelegramBot(token, options)
+
+
 
 console.log('bot ready! ')
